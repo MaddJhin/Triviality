8165cc4205bb3a0711e55ddfcf835fd9f43a3c9b
Francisco Carrera <franciscocarrer@gmail.com>
2017-08-28 20:20:48 -0400

    Added Trivia Object constructor with test trivia. Trivia organized in array

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -1,1 +1,19 @@
-console.log("Testing Link");
+function trivia(prompt, askA, askB, askC, askD, answer){
+    this.question = prompt;
+    this.optionA = askA;
+    this.optionB = askB;
+    this.optionC = askC;
+    this.optionD = askD;
+    this.correctAnswer = answer;
+}
+
+var testQuestion = new trivia("What Is the Best Game", "Destiny", "Overwatch", "HotS", "Warframe", 1);
+var testQuestion2 = new trivia("Where Am I?", "Here", "There", "Nowhere", "Everywhere", 2);
+var testQuestion3 = new trivia("Objects are: ", "Confusing", "Awesome", "What?", "Meh", 3);
+
+var triviaArray = [
+    testQuestion,
+    testQuestion2,
+    testQuestion3
+]
+
