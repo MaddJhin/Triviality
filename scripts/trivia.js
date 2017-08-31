506fa2b71e9d632e432f9c40e114deb38aa81662
Francisco Carrera <franciscocarrer@gmail.com>
2017-08-31 00:20:32 -0400

    Added an actual question array. Modified trivia object to work with external source answer index

--- scripts/trivia.js
+++ scripts/trivia.js
@@ -6,7 +6,7 @@ function trivia(prompt, askA, askB, askC, askD, answer){
         askC,
         askD,
     ],
-    this.answerIndex = answer;
+    this.answerIndex = answer - 1;
 
     this.GetAnswer = function(){
         return this.answers[this.answerIndex];
@@ -17,16 +17,46 @@ var testQuestion = new trivia("What Is the Best Game", "Destiny", "Overwatch", "
 var testQuestion2 = new trivia("Where Am I?", "Here", "There", "Nowhere", "Everywhere", 2);
 var testQuestion3 = new trivia("Objects are: ", "Confusing", "Awesome", "What?", "Meh", 3);
 
+var q1 = new trivia("Eddie Valiant is a character in which animated film?", "Peter Pan", "Cinderella", "Who Framed Roger Rabbit", "Cars", 3)
+var q2 = new trivia("Who starred in the title role of Sydney Pollack's 1972 film \"Jeremiah Johnson?\"", "Steve McQueen", "Marlon Brando", "Robert Redford", "Paul Newman", 3)
+var q3 = new trivia("In \"Casino Royale\", who took over the role of James Bond?", "Daniel Craig", "Jeffrey Wright", "Bruce Willis", "Mike Meyers", 1)
+var q4 = new trivia("In 2010's \"Red,\" where do the main characters want to break into?", "CIA Headquarters", "FBI Headquarters", "The Pentagon", "The White House", 1)
+var q5 = new trivia("What was the name of Sam Peckinpah's 1978 trucker with CB-radio movie?", "Hooper", "White Lightening", "Breaker! Breaker!", "Convoy", 4)
+var q6 = new trivia("What was the first movie that co-starred Humphrey Bogart and Lauren Bacall?", "Dark Passage", "Key Largo", "To Have and Have Not", "The Big Sleep", 3)
+var q7 = new trivia("What movie had the tagline: Revenge is a dish best served cold.", "Kill Bill: Vol. 2", "Spider-Man 2", "Crash ", "Shrek 2", 1)
+var q8 = new trivia("What film had this line: \"Do you really think you have a chance against us, Mister Cowboy?\"", "The Princess Bride", "Top Gun", "Batman", "Die Hard", 4)
+var q9 = new trivia("Who played the part of Edward Cullen in the \"Twilight\" movie series?", "Taylor Lautner", "Peter Facinelli", "Michael Sheen", "Robert Pattinson", 4)
+var q10 = new trivia("In \"Anchorman 2: The Legend Continues\", what is the name of Ron's wife?", "Veronica", "Meagan", "Linda", "Chani", 1)
+var q11 = new trivia("Which of the following film directors never won an Academy Award for Best Director?", "Robert Zemeckis", "Steven Spielberg", "Stanley Kubrick", "Francis Ford Coppola", 3)
+var q12 = new trivia("Cheech Marin and Tommy Chong's first film together was what 1978 stoner flick?", "Things Are Tough All Over", "The Boys From Brazil", "Nice Dreams", "Up in Smoke", 4)
+var q13 = new trivia("Who played Tony Stark in \"Iron Man\"?", "Jeff Bridges", "John Goldman", "Robert Downey, Jr.", "Toby McGuire", 3)
+var q14 = new trivia("In \"Paranormal Activity 2,\" who is supposedly trying to steal her nephew Hunter?", "Susie", "Katie", "Shelly", "Stephanie", 2)
+var q15 = new trivia("What Oscar-winning actress starred in 2001's \"Lara Croft: Tomb Raider\"? ", "Angelina Jolie", "Charlize Theron", "Susan Sarandon", "Halle Berry", 1)
+var q16 = new trivia("What was Humphrey Bogart's character named in \"The Maltese Falcon\"?", "Philip Marlowe", "Charlie Allnut", "Sam Spade", "Harry Morgan",3)
+
 var questionBank = [
-    testQuestion,
-    testQuestion2,
-    testQuestion3
-]
+    q1,
+    q2,
+    q3,
+    q4,
+    q5,
+    q6,
+    q7,
+    q8,
+    q9,
+    q10,
+    q11,
+    q12,
+    q13,
+    q14,
+    q15,
+    q16
+];
 
 // Variables used for initializing and resetting after changes
-var remainingQuestions = 3;
-var questionInterval = 1000;
-var resultInterval = 5;
+var remainingQuestions = 10;
+var questionInterval = 30;
+var resultInterval = 3;
 
 // Variables used for tracking changes
 var count = questionInterval;
@@ -49,7 +79,7 @@ function SetOptions() {
         button.html("<p>" + currentTrivia.answers[i] + "</p>");
 
         // If the answer's index matches the correct answers index
-        if (currentTrivia.answerIndex == i){
+        if ((currentTrivia.answerIndex) == i){
             // On Click trigger correct answer
             button.on("click", function () {
                 console.log("Right Button Clicked!");
@@ -78,14 +108,16 @@ function AskTrivia(){
     currentTrivia = SelectTrivia();
     console.log("Current Question", currentTrivia.question);
 
-    // Empty question field of previous quesions and add the new question
+    // Empty question field of previous questions and add the new question
     $('#question').empty().append("<h3>" + currentTrivia.question + "</h3>");
 
     // Set the new options based on current trivia
     SetOptions(currentTrivia);
 
     // Start interval with global variable
+    $('#timer').html("Time Remaining: " + count + " seconds");
     timer = setInterval(Countdown, 1000);
+
 }
 
 function TimeUp() {
@@ -93,10 +125,11 @@ function TimeUp() {
     clearInterval(timer);
     ClearOptions();
     console.log("Time's Up!!!");
+    $('#timer').html("TIMES UP!");
 
     // Tell the player the right answer
     var correctAnswer = currentTrivia.GetAnswer();
-    $("#question").html("The Correct Answer Was" + correctAnswer);
+    $("#question").html("<h2>The Correct Answer Was" + correctAnswer+"</h2>");
     console.log("The Correct Answer Was", correctAnswer);
 
     // Display next Question after a few seconds if more questions remain
@@ -110,11 +143,11 @@ function CorrectAnswer() {
     // Stop Countdown Timer and clear options
     clearInterval(timer);
     ClearOptions();
+    $('#timer').html("");
 
     // Congratulate Player
-    $("#question").html("Congratulations!!! That's Correct!");
+    $("#question").html("<h2>Congratulations!!! That's Correct!</h2>");
     console.log("Congratulations!!! That's Correct!");
-
 
     // Display next Question after a few seconds if more questions remain
     if(remainingQuestions > 0) {
@@ -126,14 +159,14 @@ function WrongAnswer() {
     // Stop Countdown Timer and clear options
     clearInterval(timer);
     ClearOptions();
-    
-    // Tell Player They Chose Wrong
-    $("#question").html("Boooo!!! That's Wrong");
-    console.log("Boooo!!! That's Wrong");
+    $('#timer').html("");    
 
     // Show correct correct answer
     var correctAnswer = currentTrivia.GetAnswer();
     console.log("The Correct Answer Was", correctAnswer);
+
+    // Tell Player They Chose Wrong
+    $("#question").html("<h2>That's Wrong. The Correct Answer Was: " + correctAnswer + "</h2>");
 
     // Display next Question after a few seconds if more questions remain
     if(remainingQuestions > 0) {
@@ -148,21 +181,23 @@ function ClearOptions(){
 
 function Countdown(){
     count--;
+    console.log(count);
+    $('#timer').html("Time Remaining: " + count + " seconds");
+    
     if (count <= 0){
         // Times Up!
         console.log("Times UP!");
         TimeUp();
     }
-
-    // TO DO: 
-    // Display current time 
-    console.log(count);
 }
+
+
 
 $( document ).ready(function() {
 
     $('#btn-start').on("click", function() {
         AskTrivia();
+        $(this).hide();
         console.log("Starting Trivia!!!");
     });
 
